67ff1a64c173a4f8e836a47e81c0e469842f8e31
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-08-13 19:49:58 -0400

    Hide API token

--- air-pollution.mjs
+++ air-pollution.mjs
@@ -1,5 +1,7 @@
+import { token } from "./config.mjs";
+
 const fetchAP = async (lat, long) => {
-    const response = await fetch('http://api.openweathermap.org/data/2.5/air_pollution?lat=' + lat + '&lon=' + long + '&appid=d551fec40b9480c7f0b06104a6cad798', { mode: 'cors' })
+    const response = await fetch('http://api.openweathermap.org/data/2.5/air_pollution?lat=' + lat + '&lon=' + long + '&appid=' + token + '', { mode: 'cors' })
     const data = await response.json();
     const aqi = data.list
     return aqi
