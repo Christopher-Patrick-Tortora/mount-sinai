d92de8f8c0b9a9911b942c3d8ae6cb1bc91e70cf
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-08-13 18:21:56 -0400

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,25 @@
+//Create map of america
+//Add mount sinai location layer
+//Display mount sinai location card on click
+//Add state pollution layer
+//Display pollution info on hover
+
+import { createMap } from "./map.mjs";
+
+createMap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
