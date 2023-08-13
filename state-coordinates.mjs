dee9f96ac003ac3cb51ef84375df79c7e248be88
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-08-13 18:23:17 -0400

    Initial commit

---
+++ state-coordinates.mjs
@@ -0,0 +1,212 @@
+const stateCoords = {
+    "Alaska" :{
+      "lat":61.3850,
+      "long":-152.2683
+    },
+    "Alabama" :{
+      "lat":32.7990,
+      "long":-86.8073
+    },
+    "Arkansas" :{
+      "lat":34.9513,
+      "long":-92.3809
+    },
+    "Arizona":{
+      "lat":33.7712,
+      "long":-111.3877
+    },
+    "California" :{
+      "lat":36.1700,
+      "long":-119.7462
+    },
+    "Colorado" :{
+      "lat":39.0646,
+      "long":-105.3272
+    },
+    "Connecticut":{
+      "lat":41.5834,
+      "long":-72.7622
+    },
+    "Delaware":{
+      "lat":39.3498,
+      "long":-75.5148
+    },
+    "Florida" :{
+      "lat":27.8333,
+      "long":-81.7170
+    },
+    "Georgia" :{
+      "lat":32.9866,
+      "long":-83.6487
+    },
+    "Hawaii" :{
+      "lat":21.1098,
+      "long":-157.5311
+    },
+    "Iowa" :{
+      "lat":42.0046,
+      "long":-93.2140
+    },
+    "Idaho" :{
+      "lat":44.2394,
+      "long":-114.5103
+    },
+    "Illinois" :{
+      "lat":40.3363,
+      "long":-89.0022
+    },
+    "Indiana" :{
+      "lat":39.8647,
+      "long":-86.2604
+    },
+    "Kansas" :{
+      "lat":38.5111,
+      "long":-96.8005
+    },
+    "Kentucky" :{
+      "lat":37.6690,
+      "long":-84.6514
+    },
+    "Louisiana" :{
+      "lat":31.1801,
+      "long":-91.8749
+    },
+    "Massachusetts" :{
+      "lat":42.2373,
+      "long":-71.5314
+    },
+    "Maryland" :{
+      "lat":39.0724,
+      "long":-76.7902
+    },
+    "Maine" :{
+      "lat":44.6074,
+      "long":-69.3977
+    },
+    "Michigan":{
+      "lat":43.3504,
+      "long":-84.5603
+    },
+    "Minnesota":{
+      "lat":45.7326,
+      "long":-93.9196
+    },
+    "Missouri" :{
+      "lat":38.4623,
+      "long":-92.3020
+    },
+    "Mississippi":{
+      "lat":32.7673,
+      "long":-89.6812
+    },
+    "Montana":{
+      "lat":46.9048,
+      "long":-110.3261
+    },
+    "North Carolina":{
+      "lat":35.6411,
+      "long":-79.8431
+    },
+    "North Dakota":{
+      "lat":47.5362,
+      "long":-99.7930
+    },
+    "Nebraska":{
+      "lat":41.1289,
+      "long":-98.2883
+    },
+    "New Hampshire":{
+      "lat":43.4108,
+      "long":-71.5653
+    },
+    "New Jersey":{
+      "lat":40.3140,
+      "long":-74.5089
+    },
+    "New Mexico":{
+      "lat":34.8375,
+      "long":-106.2371
+    },
+    "Nevada":{
+      "lat":38.4199,
+      "long":-117.1219
+    },
+    "New York":{
+      "lat":42.1497,
+      "long":-74.9384
+    },
+    "Ohio":{
+      "lat":40.3736,
+      "long":-82.7755
+    },
+    "Oklahoma":{
+      "lat":35.5376,
+      "long":-96.9247
+    },
+    "Oregon":{
+      "lat":44.5672,
+      "long":-122.1269
+    },
+    "Pennsylvania":{
+      "lat":40.5773,
+      "long":-77.2640
+    },
+    "Rhode Island":{
+      "lat":41.6772,
+      "long":-71.5101
+    },
+    "South Carolina":{
+      "lat":33.8191,
+      "long":-80.9066
+    },
+    "South Dakota":{
+      "lat":44.2853,
+      "long":-99.4632
+    },
+    "Tennessee":{
+      "lat":35.7449,
+      "long":-86.7489
+    },
+    "Texas":{
+      "lat":31.1060,
+      "long":-97.6475
+    },
+    "Utah":{
+      "lat":40.1135,
+      "long":-111.8535
+    },
+    "Virginia":{
+      "lat":37.7680,
+      "long":-78.2057
+    },
+    "Vermont":{
+      "lat":44.0407,
+      "long":-72.7093
+    },
+    "Washington":{
+      "lat":47.3917,
+      "long":-121.5708
+    },
+    "Wisconsin":{
+      "lat":44.2563,
+      "long":-89.6385
+    },
+    "West Virginia":{
+      "lat":38.4680,
+      "long":-80.9696
+    },
+    "Wyoming":{
+      "lat":42.7475,
+      "long":-107.2085
+    },
+    "District of Columbia":{
+        "latitude":38.8974,
+        "longitude":-77.0268
+      },
+      "Puerto Rico":{
+        "latitude":18.2491,
+        "longitude":-66.628
+      }
+}
+
+export { stateCoords }
