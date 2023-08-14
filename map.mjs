407df9dd73ac14c4e8e7fdf0a0e59e100891caee
Christopher Patrick Tortora <ctortora42@gmail.com>
2023-08-13 21:14:14 -0400

    Display info

--- map.mjs
+++ map.mjs
@@ -47,9 +47,20 @@ const interaction = (map) => {
     };
 
     info.update = function (props) {
-        this._div.innerHTML = '<h4>US Air Pollution</h4>' + (props ?
-            '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-            : 'Hover over a state');
+        this._div.innerHTML = 
+        '<h4>US Air Pollution</h4>' 
+        + (props ? 
+        '<b>' + props.name + '</b><br />' 
+        + `<p>Air Quality Index: ${props.aqi}</p>`
+        + `<p>SO<sub>2</sub>: ${props.components.so2}</p>`
+        + `<p>NO<sub>2</sub>: ${props.components.no2}</p>`
+        + `<p>PM<sub>10</sub>: ${props.components.pm10}</p>`
+        + `<p>PM<sub>2.5</sub>: ${props.components.pm2_5}</p>`
+        + `<p>O<sub>3</sub>: ${props.components.o3}</p>`
+        + `<p>CO: ${props.components.co}</p>`
+        + `<p>NH<sub>3</sub>: ${props.components.nh3}</p>`
+        + `<p>NO: ${props.components.no}</p>`
+        : 'Hover over a state');
     };
 
     function highlightFeature(e) {
@@ -67,6 +78,7 @@ const interaction = (map) => {
         }
 
         info.update(layer.feature.properties);
+        console.log(layer.feature.properties)
     }
 
     function resetHighlight(e) {
